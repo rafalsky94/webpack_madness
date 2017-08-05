5218a7da9df3f6aed5bd1a8da3527bbbb1d5f763
Jakub Wasiljew <websolutionslab@gmail.com>
2017-08-06 00:55:42 +0200

    Production env in production

--- app/app.js
+++ app/app.js
@@ -1,12 +1,32 @@
 import _ from 'lodash';
+import printMe from './print.js';
+import './components/styles/main.css';
 
 function component() {
-    let element = document.createElement('div');
 
-    // Lodash, now imported by this script
+    let element = document.createElement('div');
+    let btn = document.createElement('button');
+
     element.innerHTML = _.join([ 'Hello', 'webpack' ], ' ');
 
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
+
     return element;
+}
+
+let element = component(); // Store the element to re-render on print.js changes
+
+
+if ( module.hot ) {
+    module.hot.accept('./print.js', function () {
+        console.log('Accepting the updated printMe module!');
+        document.body.removeChild(element);
+        element = component(); // Re-render the "component" to update the click handler
+        document.body.appendChild(element);
+    })
 }
 
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,60 @@
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 
 module.exports = {
-    entry: './app/app.js',
+    entry: {
+        app: './app/app.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: [ 'es2015', 'es2017' ]
+                }
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin([ 'dist' ]),
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        }),
+        new webpack.HotModuleReplacementPlugin()
+
+    ],
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     }
 };
